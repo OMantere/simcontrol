92e2533031a718135836dbd96a48c9eb31a78036
Oskari Mantere <osku.mantere@gmail.com>
2019-02-23 11:41:38 +0200

    basic image-pose collector

--- scripts/data_generator.py
+++ scripts/data_generator.py
@@ -20,30 +20,22 @@ from sensor_msgs.msg import Image
 class Node(object):
     def __init__(self):
         rospy.init_node('train_gen', anonymous=True)
-        self.ir_beacons = rospy.Subscriber('/uav/camera/left/ir_beacons', IRMarkerArray, self.ir_subscriber)
-        self.rgb_right = rospy.Subscriber('/uav/camera/right/image_rect_color', Image, self.image_right_subscriber)
+        self.tf_listener = tf.TransformListener()
         self.rgb_left = rospy.Subscriber('/uav/camera/left/image_rect_color', Image, self.image_left_subscriber)
-        self.latest_markers = defaultdict()
-        self.stamps = defaultdict(dict)
+        self.left_images {}
+        self.x = {}
+        self.q = {}
         rospy.spin()
 
-    def ir_subscriber(self, msg):
-        self.latest_markers = defaultdict(dict)
-        self.latest_markers_time = msg.header.stamp
-        print('got ir beacons with stamp', msg.header.stamp)
-        for marker in msg.markers:
-            self.latest_markers[marker.landmarkID.data][marker.markerID.data] = np.array([marker.x, marker.y])
-
     def image_left_subscriber(self, msg):
-        self.latest_left_time = msg.header.stamp
-        self.latest_left = msg.data
+        translation, rotation = self.tf_listener.lookupTransform('world','uav/imu', msg.header.stamp)
+        q = np.quaternion(rotation[3], rotation[0], rotation[1], rotation[2])
+        x = np.array(translation)
+        nsec = msg.header.stamp.to_sec
+        self.left_images[nsec] = np.fromstring(image_msg.data, np.uint8)
+        self.x[nsec] = x
+        self.q[nsec] = q
         print('got left rgb with stamp', msg.header.stamp)
-
-    def image_right_subscriber(self, msg):
-        self.latest_right_time = msg.header.stamp
-        self.latest_right = msg.data
-        print('got right rgb with stamp', msg.header.stamp)
-
 
 if __name__ == '__main__':
     try:
