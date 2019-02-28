1718559cc47015fa675d4186835635ab23680e28
Kenneth Blomqvist <kekeblom@gmail.com>
2019-02-28 16:51:10 +0200

    use OSQP and condensed LMPC formulation

--- scripts/mpc.py
+++ scripts/mpc.py
@@ -1,47 +1,63 @@
 import time
-import cvxpy
 import numpy as np
-import cvxpy as cp
-from cvxpy import Variable
+import osqp
+import numpy as np
+import scipy as sp
+import scipy.sparse as sparse
+
 
 class MPC(object):
-    def __init__(self, dynamics, Q, R):
+    def __init__(self, dynamics):
         self.dynamics = dynamics
-        self.Q = Q
-        self.R = R
+	# Objective function
+	self.Q = sparse.diags([10., 0.])
+	self.R = 0.1 * sparse.eye(1)
 
-    def solve(self, trajectory, x_0):
-        A, B = self.dynamics.linearize()
-        n = self.dynamics.dim_x
-        m = self.dynamics.dim_u
+    def solve(self, reference, x0):
+	N = 25 # Prediction horizon
 
-        T = trajectory.shape[1]
-        x = Variable((n, T+1))
-        u = Variable((m, T))
+        Ad, Bd = self.dynamics.linearize()
+        nx = self.dynamics.dim_x
+        nu = self.dynamics.dim_u
 
-        constraints = []
-        cost = 0.0
-        for t in range(T):
-            constraints += [x[:, t+1] == A * x[:, t] + B * u[:, t]]
+	# - quadratic objective
+	P = sparse.block_diag([sparse.kron(sparse.eye(N), self.Q), self.Q,
+			       sparse.kron(sparse.eye(N), self.R)]).tocsc()
+	# - linear objective
+	q = np.hstack([np.kron(np.ones(N), -self.Q.dot(reference)), -self.Q.dot(reference),
+		       np.zeros(N*nu)])
+	# - linear dynamics
+	Ax = sparse.kron(sparse.eye(N+1),-sparse.eye(nx)) + sparse.kron(sparse.eye(N+1, k=-1), Ad)
+	Bu = sparse.kron(sparse.vstack([sparse.csc_matrix((1, N)), sparse.eye(N)]), Bd)
+	Aeq = sparse.hstack([Ax, Bu])
+	leq = np.hstack([-x0, np.zeros(N*nx)])
+	ueq = leq
+	# - input and state constraints
+	umin = np.ones(nu) * -1.0
+	umax = np.ones(nu) * 1.0
+	xmin = np.ones(nx) * -100.0
+	xmax = np.ones(nx) * 100.0
+	Aineq = sparse.eye((N+1)*nx + N*nu)
+	lineq = np.hstack([np.kron(np.ones(N+1), xmin), np.kron(np.ones(N), umin)])
+	uineq = np.hstack([np.kron(np.ones(N+1), xmax), np.kron(np.ones(N), umax)])
+	# - OSQP constraints
+	A = sparse.vstack([Aeq, Aineq]).tocsc()
+	l = np.hstack([leq, lineq])
+	u = np.hstack([ueq, uineq])
 
-            x_diff = cp.abs(x[:, t] - trajectory[:, t])[0]
-            cost += cp.sum_squares(x_diff) + cp.sum_squares(u[:, t])
-            # cost += cp.quad_form(x_diff[:, None], self.Q)
-            # cost += cp.quad_form(u[:, t][:, None], self.R)
+	# Create an OSQP object
+	prob = osqp.OSQP()
 
-        constraints += [cp.abs(u) <= 1.0]
-        problem = cp.Problem(cp.Minimize(cost), constraints)
+	# Setup workspace
+	prob.setup(P, q, A, l, u, warm_start=True, polish=False,
+		eps_abs=1e-1, eps_rel=1e-1)
 
-        x.value = np.zeros((n, T+1))
-        for i in range(5):
-            start = time.time()
-            u.value = np.random.randn(u.shape[0], u.shape[1])
-            result = problem.solve(warm_start=True, solver=cp.OSQP,
-                    eps_abs=1e-1, eps_rel=1e-1)
-            print("Took {} to solve, cost: {}".format(time.time() - start, result))
+	res = prob.solve()
 
+	x = res.x[:N*nx].reshape(N, nx)
+	controls = res.x[-N*nu:]
+	return x, controls
 
-        return u, x, problem.value
 
 if __name__ == "__main__":
     class Dynamics(object):
@@ -58,9 +74,18 @@ if __name__ == "__main__":
     Q = np.zeros((2, 2))
     Q[0, 0] = 1.0 # Only care about position.
     R = np.eye(1) * 0.1
-    mpc = MPC(Dynamics(), Q, R)
+    mpc = MPC(Dynamics())
 
-    trajectory = np.stack([np.linspace(0, 10, 20), np.zeros((20,))])
-    x_0 = np.zeros((2, 1))
-    mpc.solve(trajectory, x_0)
+    reference = np.array([1., 0.])
+    x_0 = np.zeros(2)
+    x, u = mpc.solve(reference, x_0)
+
+    from matplotlib import pyplot
+    figure = pyplot.figure()
+    axis = figure.add_subplot(211)
+    axis.plot(np.arange(u.shape[0]), u)
+    axis = figure.add_subplot(212)
+    axis.plot(np.arange(u.shape[0]), x[:, 0])
+    pyplot.show()
+
 
